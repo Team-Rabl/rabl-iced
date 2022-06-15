099e78d92da2ed84478b2706162d3f6f423ea7bd
zach <zach@rabl.chat>
2022-06-14 23:42:17 -0500

    yuh

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use iced::{Column, Text, executor, Command, Application, Settings, Container, Length};
+use iced::{Column, Text, executor, Command, Application, Settings, Container, Length, TextInput};
 
 use rabl_iced::styles;
 
+// Messages must derive Clone
+#[derive(Debug, Clone)]
+enum Message {
+    TextInputChanged(String)
+}
+
 #[derive(Default)]
-struct Rabl;
+struct Rabl {
+    username: iced::text_input::State,
+    usr_val: String,
+    // password: iced::text_input::State,
+    // p_val: String
+}
 
 impl Application for Rabl {
     type Executor = executor::Default;
 
-    type Message = ();
+    type Message = Message;
 
     type Flags = ();
 
@@ -22,17 +33,39 @@ impl Application for Rabl {
     }
 
     fn update(&mut self, _message: Self::Message) -> iced::Command<Self::Message> {
+        // println!("{:?}", _message);
+        match _message {
+            Message::TextInputChanged(s) => {
+                self.usr_val = s
+            },
+        };
+
         Command::none()
     }
 
     fn view(&mut self) -> iced::Element<'_, Self::Message> {
+        let inp = TextInput::new(
+            &mut self.username, 
+            "Username", 
+            &self.usr_val, 
+            Message::TextInputChanged
+        );
+
+        
         // App content
         let app_content = Column::new()
             .spacing(20)
             .padding(20)
             .push(Text::new(String::from("Rabl Login")))
-            .push(Text::new(String::from("Username:")))
-            .push(Text::new(String::from("Password:")));
+            .push(
+                iced::Row::new()
+                .push(Text::new(String::from("Username:")))
+                .push(inp)
+            )
+            .push(
+                iced::Row::new()
+                .push(Text::new(String::from("Password:")))
+            );
 
         // Main app container
         Container::new(app_content)
